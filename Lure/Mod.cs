19b6f3c7b1077819f71202738801ece7073e313c
Jade <asleepydoggy@gmail.com>
2024-11-09 06:22:14 +1100

    Remove patches

--- Lure/Mod.cs
+++ Lure/Mod.cs
@@ -6,22 +6,10 @@ namespace Sulayre.Lure;
 public class Mod : IMod {
     public Config Config;
 
-    public Mod(IModInterface modInterface) {
+    public Mod(IModInterface modInterface)
+    {
         this.Config = modInterface.ReadConfig<Config>();
-/*        modInterface.Logger.Information("\n88     88   88 88\"\"Yb 888888 \r\n88     88   88 88__dP 88__   \r\n88  .o Y8   8P 88\"Yb  88\"\"   \r\n88ood8 `YbodP' 88  Yb 888888 ");
-		modInterface.RegisterScriptMod(new GlobalPatcher());
-		modInterface.RegisterScriptMod(new PatternPatcher());
-		modInterface.RegisterScriptMod(new FacePatcher());
-		modInterface.RegisterScriptMod(new BarkPatcher());
-        modInterface.RegisterScriptMod(new PropPatcher());
-		modInterface.RegisterScriptMod(new ItemPatcher());
-		modInterface.RegisterScriptMod(new SavePatcher());
-		modInterface.RegisterScriptMod(new TitlePatcher());
-		modInterface.RegisterScriptMod(new NetworkPatcher());
-        modInterface.RegisterScriptMod(new MainMenuPatcher());
-		modInterface.RegisterScriptMod(new ServerButtonPatcher());
-		modInterface.RegisterScriptMod(new JournalPatcher());
-		modInterface.RegisterScriptMod(new AquaPatcher());*/
+        modInterface.Logger.Information("\n88     88   88 88\"\"Yb 888888 \r\n88     88   88 88__dP 88__   \r\n88  .o Y8   8P 88\"Yb  88\"\"   \r\n88ood8 `YbodP' 88  Yb 888888 ");
 	}
 
     public void Dispose() {
