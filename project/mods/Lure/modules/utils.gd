76c239e87ef1ed2d2dc74f1f9f18e01b5ac5a11e
Jade ・:*・。*・ <asleepydoggy@gmail.com>
2024-11-14 12:50:17 +1100

    Add b, i, u, s, and bgcolor tags to pretty print

--- project/mods/Lure/modules/utils.gd
+++ project/mods/Lure/modules/utils.gd
@@ -1,30 +1,72 @@
 extends Reference
 
 
-# Convert bbcode color tags to ANSI escape codes and print to the terminal
+# Convert bbcode tags to ANSI escape codes and print to the terminal
+# Supports [b], [i], [u], [s], [color]/[fgcolor], and [bgcolor]
 static func pretty_print(message: String) -> void:
-	if (OS.has_feature("editor")):
-		print(message)
-		return
-	
+	var editor := OS.has_feature("editor")
+	var csi := PoolByteArray([0x1b]).get_string_from_ascii() + "[" # \e[
+	var reset := csi + "m" # \e[m
 	var formatted_message := message
 	
-	var colour_tag_regex := RegEx.new()
-	colour_tag_regex.compile("\\[color=#?(?<hex_code>[0-9a-fA-F]{6}|[0-9a-fA-F]{3})\\]")
-	var valid_tags := colour_tag_regex.search_all(message)
+	var bbcode_regex := RegEx.new()
+	# /\[(?<tag>\/?\w+)=?(?<options>[#\w]*\]/g
+	bbcode_regex.compile("\\[(?<tag>\\/?\\w+)=?(?<options>[#\\w]*)\\]")
+	var hex_regex := RegEx.new()
+	hex_regex.compile("#?(?<hex_code>[0-9a-fA-F]{6}|[0-9a-fA-F]{3})")
 	
-	var escape_code := PoolByteArray([0x1b]).get_string_from_ascii()
-	
-	for tag in valid_tags:
-		var rgb := hex_to_rgb(tag.get_string("hex_code")) as PoolStringArray
-		var colour_code: String = "%s[38;2;%sm" % [escape_code, rgb.join(";")]
+	for result in bbcode_regex.search_all(message):
+		var bbcode: String = result.get_string()
 		
-		formatted_message = formatted_message.replace(tag.get_string(), colour_code)
+		if editor:
+			formatted_message = formatted_message.replace(bbcode, "")
+			continue
+		
+		var tag: String = result.get_string("tag")
+		var options: String = result.get_string("options")
+		var control_sequence := csi
+		var closing := tag.begins_with("/")
+		tag.lstrip("/")
+		
+		match tag:
+			"b":
+				control_sequence += "1" if !closing else "22"
+			"i":
+				control_sequence += "3" if !closing else "23"
+			"u":
+				control_sequence += "4" if !closing else "24"
+			"s":
+				control_sequence += "9" if !closing else "29"
+			"color", "fgcolor":
+				if !closing:
+					if hex_regex.sub(options, "") != "":
+						continue
+					
+					options.lstrip("#")
+					var rgb := hex_to_rgb(options) as PoolStringArray
+					
+					control_sequence += "38;2;%s" % rgb.join(";")
+				else:
+					control_sequence += "39"
+			"bgcolor":
+				if !closing:
+					if hex_regex.sub(options, "") != "":
+						continue
+					
+					options.lstrip("#")
+					var rgb := hex_to_rgb(options) as PoolStringArray
+					
+					control_sequence += "48;2;%s" % rgb.join(";")
+				else:
+					control_sequence += "49"
+		
+		control_sequence += "m"
+		formatted_message = formatted_message.replace(bbcode, control_sequence)
 	
-	var reset_colour = escape_code + "[38;5;7m"
-	formatted_message = formatted_message.replace("[/color]", reset_colour)
+	if !editor:
+		formatted_message += reset
 	
-	print(formatted_message + reset_colour)
+	print(formatted_message)
 
 
 # Convert a hex code to an array of ints representing r, g, and b
@@ -51,6 +93,6 @@ static func hex_to_rgb(hex_code: String) -> Array:
 			codes[i] += str(codes[i])
 		
 		var hex: String = "0x" + codes[i]
-		channels.append(str(hex.hex_to_int()))
+		channels.append(hex.hex_to_int())
 	
 	return channels
